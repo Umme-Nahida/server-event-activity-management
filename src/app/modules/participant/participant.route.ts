557553ae264d50bb55109e48aa5ed16b05faa39f
Nahida Akther <ummenahida20@gmail.com>
2025-12-08 15:19:43 +0600

    participants route

---
+++ src/app/modules/participant/participant.route.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import authCookies from "../../middleware/authCookies";
+import { Role } from "../../../../prisma/generated/prisma/enums";
+import { ParticipantController } from "./participant.controller";
+
+
+const router = Router();
+
+router.post(
+  "/joint-event/:id",
+  authCookies(Role.USER, Role.HOST),
+  ParticipantController.joinEvent
+);
+
+export const ParticipantRoutes = router;

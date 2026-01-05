84969fd7e0bb58eed2bea7f76766715343e8a8c0
Nahida Akther <ummenahida20@gmail.com>
2026-01-05 20:41:16 +0600

    refactor: simplify the event searchable field

--- src/app/modules/common/event.constant.ts
+++ src/app/modules/common/event.constant.ts
@@ -1,4 +1,4 @@
 
 export const eventFilterableField = ["type","location","time","status","searchTerm", "date", "minFee", "maxFee", "fee"]
-export const eventSearchableField = ["type","location","time"]
+export const eventSearchableField = ["type","location","time","name"]
 export const eventNumberableField = ["date","fee"]
